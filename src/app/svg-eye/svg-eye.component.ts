e370b957523c44f378e9a8032d90c96881392a85
rogerkreienbuehl <r.kreienbuehl@gmx.ch>
2022-05-17 09:09:48 +0200

    fix: code improvements

--- src/app/svg-eye/svg-eye.component.ts
+++ src/app/svg-eye/svg-eye.component.ts
@@ -13,48 +13,41 @@ import {
   styleUrls: ['./svg-eye.component.css'],
 })
 export class SvgEyeComponent implements OnInit {
-  @Input() posTop = '0px'
-  @Input() posLeft = '0px'
+  @Input() posTop: string = '0px'
+  @Input() posLeft: string = '0px'
 
-  @ViewChild('iris') iris: ElementRef | undefined
-  @ViewChild('closeLid') closeLid: ElementRef | undefined
+  @ViewChild('iris') iris!: ElementRef
+  @ViewChild('closeLid') closeLid!: ElementRef
 
   private rect: DOMRect = new DOMRect()
 
-  private xo = 0
-  private yo = 0
+  private xo: number = 0
+  private yo: number = 0
 
   constructor() {}
 
   ngOnInit(): void {
     setInterval((evt: Event) => {
-      if (this.closeLid) {
-        if (this.closeLid.nativeElement.style.opacity === '1') return // do not close and then open if already closed
-        this.closeLid.nativeElement.style.opacity = '1'
-        setTimeout((evt: Event) => {
-          if (this.closeLid) this.closeLid.nativeElement.style.opacity = '0'
-        }, 300)
-      }
+      if (this.closeLid.nativeElement.style.opacity === '1') return // do not close and then open if already closed
+      this.closeLid.nativeElement.style.opacity = '1'
+      setTimeout((evt: Event) => {
+        this.closeLid.nativeElement.style.opacity = '0'
+      }, 300)
     }, 7 * 1000)
   }
 
   ngAfterViewInit(): void {
-    if (this.closeLid) {
-      this.closeLid.nativeElement.style.opacity = '0'
-    }
+    this.closeLid.nativeElement.style.opacity = '0'
 
-    if (this.iris) {
-      this.rect = this.iris.nativeElement.children[0].getBoundingClientRect()
+    this.rect = this.iris.nativeElement.children[0].getBoundingClientRect()
 
-      this.xo = this.rect.x + this.rect.width / 2 // x-origin
-      this.yo = this.rect.y + this.rect.height / 2 // y-origin
-    }
+    this.xo = this.rect.x + this.rect.width / 2 // x-origin
+    this.yo = this.rect.y + this.rect.height / 2 // y-origin
   }
 
   changeLidState() {
-    if (this.closeLid)
-      this.closeLid.nativeElement.style.opacity =
-        this.closeLid.nativeElement.style.opacity === '1' ? '0' : 1
+    this.closeLid.nativeElement.style.opacity =
+      this.closeLid.nativeElement.style.opacity === '1' ? '0' : 1
   }
 
   @HostListener('document:mousemove', ['$event'])
@@ -74,7 +67,7 @@ export class SvgEyeComponent implements OnInit {
       xe *= 0.9
       ye *= 0.9
     }
-    if (this.iris)
-      this.iris.nativeElement.style.transform = `translateX(${xe}px) translateY(${ye}px)`
+
+    this.iris.nativeElement.style.transform = `translateX(${xe}px) translateY(${ye}px)`
   }
 }
